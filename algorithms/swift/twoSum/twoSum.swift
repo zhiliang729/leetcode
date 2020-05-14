5dc2a4cd53a03dc7050d09cb0ee5d853ef698fb5
zhangliang <zhangliang@tataufo.com>
2020-05-14 11:27:06 +0800

    twosum

--- algorithms/swift/twoSum/twoSum.swift
+++ algorithms/swift/twoSum/twoSum.swift
@@ -3,19 +3,17 @@
 // Date   : 2019-03-29
 
 /********************************************************************************** 
-* 
-* Given an array of integers, find two numbers such that they add up to a specific target number.
-* 
-* The function twoSum should return indices of the two numbers such that they add up to the target, 
-* where index1 must be less than index2. Please note that your returned answers (both index1 and index2) 
-* are not zero-based.
-* 
-* You may assume that each input would have exactly one solution.
-* 
-* Input: numbers={2, 7, 11, 15}, target=9
-* Output: index1=1, index2=2
-* 
-*               
+* Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+*
+* You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*
+* Example:
+*
+*   Given nums = [2, 7, 11, 15], target = 9,
+*
+*   Because nums[0] + nums[1] = 2 + 7 = 9,
+*   return [0, 1].
+*
 **********************************************************************************/
 
 class Solution {
